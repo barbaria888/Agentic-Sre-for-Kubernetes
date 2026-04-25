39fc6f40824d74280a07966eae834aed70fd949b
HARDIK ARORA <arorahardik0811@gmail.com>
2026-04-25 20:32:56 +0530

    Update memory.py

--- agent/memory.py
+++ agent/memory.py
@@ -1,18 +1,45 @@
 import chromadb
+import time
 from chromadb.utils import embedding_functions
 
-embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
-    model_name="all-MiniLM-L6-v2"
-)
+# lazy init (DO NOT connect at import time)
+client = None
+collection = None
 
-client = chromadb.HttpClient(host="chroma", port=8000)
 
-collection = client.get_or_create_collection(
-    name="sre-memory",
-    embedding_function=embedding_func
-)
+def get_client():
+    global client, collection
+
+    if client:
+        return collection
+
+    embedding_func = embedding_functions.SentenceTransformerEmbeddingFunction(
+        model_name="all-MiniLM-L6-v2"
+    )
+
+    for i in range(15):
+        try:
+            print(f"⏳ Connecting to Chroma... attempt {i+1}")
+            client = chromadb.HttpClient(host="chroma", port=8000)
+
+            collection = client.get_or_create_collection(
+                name="sre-memory",
+                embedding_function=embedding_func
+            )
+
+            print("✅ Connected to Chroma")
+            return collection
+
+        except Exception as e:
+            print("❌ Chroma not ready, retrying...")
+            time.sleep(3)
+
+    raise Exception("Chroma connection failed after retries")
+
 
 def store_incident(pod, issue, action, result, score):
+    col = get_client()
+
     doc = f"""
     Pod: {pod['name']}
     Namespace: {pod['namespace']}
@@ -22,14 +49,17 @@ def store_incident(pod, issue, action, result, score):
     Score: {score}
     """
 
-    collection.add(
+    col.add(
         documents=[doc],
         metadatas=[{"score": score}],
-        ids=[f"{pod['name']}-{len(doc)}"]
+        ids=[f"{pod['name']}-{time.time()}"]
     )
 
+
 def search_similar(issue, k=5):
-    res = collection.query(
+    col = get_client()
+
+    res = col.query(
         query_texts=[issue],
         n_results=k
     )
@@ -37,7 +67,6 @@ def search_similar(issue, k=5):
     docs = res.get("documents", [[]])[0]
     metas = res.get("metadatas", [[]])[0]
 
-    #  attach score to each result
     enriched = []
     for d, m in zip(docs, metas):
         enriched.append({
