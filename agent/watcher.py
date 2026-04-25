64e2a93e47dcde17fcc9ba2c6cf7a9cec8ed0f7e
HARDIK ARORA <arorahardik0811@gmail.com>
2026-04-26 00:08:25 +0530

    Update watcher.py

--- agent/watcher.py
+++ agent/watcher.py
@@ -1,21 +1,14 @@
-from kubernetes import client, config, watch
-
-config.load_incluster_config()
-v1 = client.CoreV1Api()
-
-def get_reason(pod):
-    try:
-        return pod.status.container_statuses[0].state.waiting.reason
-    except:
-        return pod.status.phase
-
 def watch_pods():
     w = watch.Watch()
-    for event in w.stream(v1.list_pod_for_all_namespaces):
-        pod = event['object']
 
-        yield {
-            "name": pod.metadata.name,
-            "namespace": pod.metadata.namespace,
-            "status": get_reason(pod)
-        }
+    for event in w.stream(v1.list_pod_for_all_namespaces):
+        pod = event["object"]
+
+        try:
+            yield {
+                "name": pod.metadata.name,
+                "namespace": pod.metadata.namespace,
+                "status": pod.status.phase
+            }
+        except Exception:
+            continue
